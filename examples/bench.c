4eba5185d6ea37b357081d00b1351df5b66adf0c
Reini Urban <rurban@cpan.org>
2026-02-19 21:59:22 +0100

    extend benchmark to error counting

--- examples/bench.c
+++ examples/bench.c
@@ -11,13 +11,17 @@ extern wint_t musl_towupper(wint_t wc); /* towctrans-musl-new */
 extern wint_t musl_towlower(wint_t wc);
 extern wint_t old_towupper(wint_t wc); /* towctrans-musl-old */
 extern wint_t old_towlower(wint_t wc);
-// our workaround via locales does not work yet
+// our workaround via locales does not work
 // #define glibc_towlower towlower
 // #define glibc_towupper towupper
 extern wint_t glibc_towupper(wint_t wc); /* towctrans-glibc */
 extern wint_t glibc_towlower(wint_t wc);
 
+#define MAX_UNI 0x1ffff
+#define SIZE 1000000
+#define RETRIES 10000
 #define SZ(a) sizeof(a) / sizeof(*a)
+
 #ifndef _WIN32
 static inline long TEST_TIME(void) {
   struct timeval now;
@@ -30,40 +34,56 @@ static inline long TEST_TIME(void) { return GetTickCount(); }
 
 int main(void) {
   int i;
-  wint_t ws[10000];
+  wint_t *ws, *lw, *up;
   wint_t *ps;
   long t0, t1;
+  int errs;
   srandom(0U);
   /* prep */
+  ws = malloc(SIZE * sizeof(wint_t));
+  lw = malloc(MAX_UNI * sizeof(wint_t));
+  up = malloc(MAX_UNI * sizeof(wint_t));
   for (i = 0; i < SZ(ws); i++) {
     wint_t wc = (wint_t)(random() % 0x1ffff);
     ws[i] = wc;
   }
-  // warmup
-  ps = &ws[0];
-  for (i = 0; i < SZ(ws); i++) {
-    (void)my_towlower(*ps++);
+  /* warmup */
+  for (i = 0; i < MAX_UNI; i++) {
+    lw[i] = my_towlower(i);
+    up[i] = my_towupper(i);
   }
 
 #define BENCH(name, locasefn, upcasefn)                                        \
   t0 = TEST_TIME();                                                            \
-  ps = &ws[0];                                                                 \
-  for (i = 0; i < SZ(ws); i++) {                                               \
-    *ps = locasefn(*ps);                                                       \
-    ps++;                                                                      \
+  errs = 0;                                                                    \
+  for (int j = 0; j < RETRIES; j++) {                                          \
+    for (i = 0; i < SZ(ws); i++) {                                             \
+      wint_t wc = ws[i];                                                       \
+      wint_t n = locasefn(wc);                                                 \
+      if (n != lw[wc]) errs++;                                                 \
+    }                                                                          \
   }                                                                            \
-  ps = &ws[0];                                                                 \
-  for (i = 0; i < SZ(ws); i++) {                                               \
-    *ps = upcasefn(*ps);                                                       \
-    ps++;                                                                      \
+  for (int j = 0; j < RETRIES; j++) {                                          \
+    for (i = 0; i < SZ(ws); i++) {                                             \
+      wint_t wc = ws[i];                                                       \
+      wint_t n = upcasefn(wc);                                                 \
+      if (n != up[wc]) errs++;                                                 \
+    }                                                                          \
   }                                                                            \
   t1 = TEST_TIME();                                                            \
-  printf("%10s: %10ld [us]\n", name, t1 - t0)
+  if (errs)                                                                    \
+    printf("%10s: %10ld [us]\t%u errors\n", name, t1 - t0, errs/RETRIES);      \
+  else                                                                         \
+    printf("%10s: %10ld [us]\n", name, t1 - t0)
 
   BENCH("my", my_towlower, my_towupper);
   BENCH("musl-new", musl_towlower, musl_towupper);
   BENCH("musl-old", old_towlower, old_towupper);
   BENCH("glibc", glibc_towlower, glibc_towupper);
+  printf("\n");
 
+  free(up);
+  free(lw);
+  free(ws);
   return 0;
 }
