1b10c8a17241185b6668a7c8a82b2ed21f6e0d28
Reini Urban <rurban@cpan.org>
2026-02-24 19:15:01 +0100

    add update-bench target

---
+++ examples/update-bench.pl
@@ -0,0 +1,47 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+sub usage {
+    die "Usage: $0 gen_wctrans bench.log\n$!";
+}
+usage() if @ARGV != 2;
+
+my $bench;
+{
+    local $/;
+    open my $fh, '<', $ARGV[1] or usage();
+    my $s = <$fh>;
+    close $fh;
+    ($bench) = $s =~ m{^(\./bench.+?)^\d+ total}ms;
+}
+$bench =~ s/ towctrans-my\.o .+?glibc\.o$/ towctrans-*.o/ms;
+$bench =~ s/\n/\n    /gms;
+$bench =~ s/\n    \n/\n\n/gms;
+$bench =~ s/\n    $/\n/;
+$bench = "    " . $bench;
+
+{
+    my $found;
+    my $gen = $ARGV[0];
+    open my $in,  '<', $gen       or usage();
+    open my $out, '>', "$gen.tmp" or usage();
+    while (<$in>) {
+        if (m{^    ./bench}) {
+            $found = 1;
+            next;
+        }
+        if (m{^=head1 INSTALLATION}) {
+            $found = 0;
+            print $out $bench . "\n";
+        }
+        if ( !$found ) {
+            print $out $_;
+        }
+    }
+    close $in;
+    close $out;
+    chmod 0755, "$gen.tmp";
+    system("mv $gen.tmp $gen") == 0 or die "mv: $!";
+    unlink "$gen.tmp";
+}
